88c9bf2430f42c0ea18bcbf74a20522a06e93a59
Victoria Tassie <vic.tee646@gmail.com>
2019-09-20 23:01:34 +0100

    Initial Commit - addition

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, jsonify, flash
+import random
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+	
+@app.route('/addition', methods=["POST", "GET"])
+def addition():
+    if request.method == "POST":
+        x = int(request.form['x'])
+        y = int(request.form['y'])
+        clientAnswer = int(request.form['clientAnswer'])
+        if clientAnswer == x + y:
+            return jsonify({'res':'Correct', 'ans': x+y})
+        else:
+            return jsonify({'res':'Incorrect', 'ans': x+y})
+
+
+    # random numbers to add together
+    x = random.randint(1,50)
+    y = random.randint(1,50)
+    ans = x + y
+
+    # random wrong answers
+    options = [random.randint(1,100), random.randint(1,100), random.randint(1,100), random.randint(1,100)]
+
+    # random number to be right
+    corans = random.randint(0,3)
+
+    # assign right answer to random position
+    options[corans] = ans
+            
+    return render_template('addition.html', x=x, y=y, options=options)
+	
+if __name__ == "__main__":
+    app.run(debug=True)
