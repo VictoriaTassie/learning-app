888ee7b2e451a0b9f72891512e09c11801edd79e
Victoria Tassie <vic.tee646@gmail.com>
2019-10-22 12:29:29 +0100

    add arithmetic sequences

--- quizzes/app.py
+++ quizzes/app.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify, flash, session
 import random
 import json
+import ast
 
 # define the blueprint
 quizzes = Blueprint('quizzes', __name__, template_folder="templates")
@@ -132,3 +133,37 @@ def simplifying_indices():
     options[corans] = ans
             
     return render_template('quizzes/simplifying-indices.html', x=x, y=y, options=options, total_correct=session['total_correct'], correct_in_row=session['correct_in_row'])
+
+@quizzes.route('/arithmetic-sequences', methods=['GET','POST'])
+def arithmetic_sequences():
+
+    if request.method == 'POST':
+        a_1 = int(request.form['x'])
+        d = int(request.form['y'])
+        clientAnswer = ast.literal_eval(request.form['clientAnswer'])
+        if clientAnswer['a1'] == a_1 and clientAnswer['d'] == d:
+            session['total_correct'] += 1
+            session['correct_in_row'] += 1
+            return jsonify({'res':'Correct', 'ans': 'a1 = '+ a_1 + ' d = ' + d, 'total_correct': session['total_correct'], 'correct_in_row': session['correct_in_row']})
+        else:
+            session['correct_in_row'] = 0
+            return jsonify({'res':'Incorrect', 'ans': 'a<sub>1</sub> = '+ str(a_1) + ', d = ' + str(d), 'total_correct': session['total_correct'], 'correct_in_row': session['correct_in_row']})
+
+
+    a_1 = random.randint(-100, 100)
+    d = random.randint(-15, 15)
+    ns = random.sample(range(2, 25), 2)
+
+    values = [a_1 + (ns[i] - 1)*d for i in range(2)]
+    ans = {'a1': a_1, 'd': d}
+
+    a = random.sample(range(a_1-5, a_1+5), 4)
+    ds = random.sample(range(d-5,d+5), 4)
+
+    options = [{'a1': a[0], 'd': ds[0]}, {'a1': a[1], 'd': ds[1]}, {'a1': a[2], 'd': ds[2]}, {'a1': a[3], 'd': ds[3]}]
+    # random number to be right
+    corans = random.randint(0,3)
+
+    options[corans] = ans
+
+    return render_template('quizzes/arithmetic-sequences.html', x=a_1, y=d, ns=ns, values=values, options=options)
